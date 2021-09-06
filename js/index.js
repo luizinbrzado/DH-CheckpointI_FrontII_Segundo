f13985b4ef7d1c39a92d02316dd3f288e2abbc7b
luizinbrzado <luiztrineves@gmail.com>
2021-09-06 16:11:32 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,76 @@
+const formFofoca = document.querySelector('.formFofoca');
+const btnAddCard = document.getElementById('addCard');
+const btnAddFormFofoca = document.getElementById('addFofoca');
+const btnFecharFormFofoca = document.getElementById('btnFechar')
+const cards = document.querySelector('.cards');
+
+btnAddCard.addEventListener('click', (e) => {
+    e.preventDefault()
+    let div = document.createElement("div");
+    div.setAttribute("class", 'divfofoca');
+
+    const title = document.querySelector("#title");
+    const desc = document.querySelector("#description");
+    const imgURL = document.querySelector("#imgURL");
+    const cards = document.querySelector('.cards');
+
+    // VALIDAÇÃO DO TÍTULO
+    if (title.value == '') {
+        title.style.border = '2px solid #f00';
+    } else {
+        title.style.border = '2px solid #f3f3f3';
+    }
+
+    // VALIDAÇÃO DA DESCRIÇÃO
+    if (desc.value == '') {
+        desc.style.border = '2px solid #f00';
+    } else {
+        desc.style.border = '2px solid #f3f3f3';
+    }
+
+    // VALIDAÇÃO DA IMAGEM
+    if (imgURL.value == '') {
+        imgURL.style.border = '2px solid #f00';
+    } else {
+        imgURL.style.border = '2px solid #f3f3f3';
+    }
+
+    // COLOCA DADOS NO CARD
+    if (title.value !== '' && desc.value !== '' && imgURL.value !== '') {
+        div.innerHTML = `
+            <img src="${imgURL.value}" alt="${title.value}">
+            <h2>${title.value.toUpperCase()}</h2>
+            <p>${desc.value}</p>
+            `
+
+        cards.appendChild(div);
+
+        title.value = "";
+        desc.value = "";
+        imgURL.value = "";
+
+        title.focus();
+    }
+})
+
+btnAddFormFofoca.addEventListener('click', () => {
+    const formFofoca = document.querySelector('.formFofoca');
+    formFofoca.style.display = 'block';
+
+    const title = document.querySelector("#title");
+    const desc = document.querySelector("#description");
+    const imgURL = document.querySelector("#imgURL");
+
+    title.value = '';
+    title.style.border = '2px solid #f3f3f3';
+
+    desc.value = '';
+    desc.style.border = '2px solid #f3f3f3';
+
+    imgURL.value = '';
+    imgURL.style.border = '2px solid #f3f3f3';
+})
+
+btnFecharFormFofoca.addEventListener('click', () => {
+    formFofoca.style.display = 'none';
+})
---
+++ js/padrao.js
@@ -0,0 +1,26 @@
+/* HEADER */
+
+let header = `
+<a href="./index.html"><img src="./img/fofoca.jpg" alt=""></a>
+<ul>
+    <button id="addFofoca">Criar fofoca</button>
+    <a href="./equipe.html"><li>Equipe</li></a>
+    <a href="./contato.html"><li>Contato</li></a>
+</ul>
+`;
+
+document.body.querySelector('header').innerHTML = header;
+
+/* /HEADER */
+
+
+
+/* FOOTER */
+
+let footer = `
+<p>© Checkpoint I - Front End II</p>
+`;
+
+document.body.querySelector('footer').innerHTML = footer;
+
+/* /FOOTER */
