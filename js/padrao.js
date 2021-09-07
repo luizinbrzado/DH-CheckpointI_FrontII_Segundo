c299994347f51c4af22d4e2403d399dfd198bb38
luizinbrzado <luiztrineves@gmail.com>
2021-09-07 17:27:43 -0300

    Mudanças na barra de navegação

--- js/padrao.js
+++ js/padrao.js
@@ -1,21 +1,29 @@
 /* HEADER */
 
 let header = `
-<a href="./index.html"><img src="./img/fofoca.jpg" alt=""></a>
-<div>
-    <a id="index" href="./equipe.html">Equipe</a>
-    <a href="./contato.html">Contato</a>
-</div>
+<nav>
+    <a href="./index.html"><img src="./img/fofoca.jpg" alt=""></a>
+    <div>
+        <ul>
+            <li><a id="index" href="./equipe.html">Equipe</a></li>
+            <li><a href="./contato.html">Contato</a></li>
+        </ul>
+    </div>
+</nav>
 `;
 
 if(window.location.href.includes('index')) {
     header = `
-    <a href="./index.html"><img src="./img/fofoca.jpg" alt=""></a>
-    <div>
-        <button id="addFofoca">Criar fofoca</button>
-        <a id="index" href="./equipe.html">Equipe</a>
-        <a href="./contato.html">Contato</a>
-    </div>`
+    <nav>
+        <a href="./index.html"><img src="./img/fofoca.jpg" alt=""></a>
+        <div>
+            <button id="addFofoca">Criar fofoca</button>
+            <ul>
+                <li><a id="index" href="./equipe.html">Equipe</a></li>
+                <li><a href="./contato.html">Contato</a></li>
+            </ul>
+        </div>
+    </nav>`
 }
 
 document.body.querySelector('header').innerHTML = header;
